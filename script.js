ffdd5c54bd1047d7b839dc46ce0df3b4e583825f
harahujo-trybe <150490155+harahujo-trybe@users.noreply.github.com>
2024-01-03 21:44:43 -0300

    Atualizar o script.js

---
+++ script.js
@@ -0,0 +1,29 @@
+// Criar um objeto XMLHttpRequest
+var xhr = new XMLHttpRequest();
+
+// Definir o tipo de resposta como JSON
+xhr.responseType = "json";
+
+// Abrir uma conexão com o arquivo .php
+xhr.open("GET", "data.php");
+
+// Enviar a requisição
+xhr.send();
+
+// Esperar pela resposta
+xhr.onload = function() {
+    // Verificar se a resposta foi bem sucedida
+    if (xhr.status == 200) {
+        // Obter os dados da resposta
+        var data = xhr.response;
+
+        // Criar elementos HTML com os dados
+        var container = document.getElementById("container");
+        for (var i = 0; i < data.length; i++) {
+            var item = document.createElement("div");
+            item.className = "item";
+            item.textContent = data[i].name + ": " + data[i].value;
+            container.appendChild(item);
+        }
+    }
+};
